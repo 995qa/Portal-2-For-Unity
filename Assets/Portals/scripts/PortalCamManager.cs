fd2e15e6b243e770e12f3a5402e5838b711fad6e
Drake-Rochelle <drakerochelle.3ds@gmail.com>
2025-10-29 18:25:19 -0700

    Portals working
    Portals working

---
+++ Assets/Portals/scripts/PortalCamManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PortalCamManager : MonoBehaviour 
+{
+    [SerializeField] private Transform playerCam;
+    [SerializeField] private Transform otherPortal;
+    [SerializeField] private Transform thisPortal;
+    private Vector3 DTP;
+
+    void Update() 
+	{
+        DTP = thisPortal.position - otherPortal.position;
+        transform.position = playerCam.position;
+        transform.position += DTP;
+        transform.rotation = Quaternion.LookRotation(-playerCam.position + otherPortal.position);
+        float distance = (playerCam.position - otherPortal.position).magnitude;
+        Debug.Log(this.name + ": " + distance);
+        this.GetComponent<Camera>().nearClipPlane = distance-0.3f;
+        this.GetComponent<Camera>().fieldOfView = 60 / (distance + 0.15f);
+    }
+}
