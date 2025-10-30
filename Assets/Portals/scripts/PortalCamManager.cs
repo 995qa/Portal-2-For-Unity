5e276c9190d0bc94450a4d8faad4eea8399e2767
Drake-Rochelle <drakerochelle.3ds@gmail.com>
2025-10-30 14:56:41 -0700

    welkfnerg
    welkfnerg

--- Assets/Portals/scripts/PortalCamManager.cs
+++ Assets/Portals/scripts/PortalCamManager.cs
@@ -6,16 +6,21 @@ public class PortalCamManager : MonoBehaviour
     [SerializeField] private Transform otherPortal;
     [SerializeField] private Transform thisPortal;
     private Vector3 DTP;
+    private Quaternion DTR;
 
     void Update() 
 	{
+        Quaternion thisRot = thisPortal.rotation;
+        Quaternion otherRot = otherPortal.rotation;
         DTP = thisPortal.position - otherPortal.position;
+        DTR = thisRot * Quaternion.Inverse(otherRot);
         transform.position = playerCam.position;
         transform.position += DTP;
-        transform.rotation = Quaternion.LookRotation(-playerCam.position + otherPortal.position);
+        transform.rotation = Quaternion.LookRotation(playerCam.position - otherPortal.position);
+        transform.rotation *= DTR;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
         float distance = (playerCam.position - otherPortal.position).magnitude;
-        Debug.Log(this.name + ": " + distance);
-        this.GetComponent<Camera>().nearClipPlane = distance-0.3f;
-        this.GetComponent<Camera>().fieldOfView = 60 / (distance + 0.15f);
+        this.GetComponent<Camera>().nearClipPlane = distance - 0.3f;
+        this.GetComponent<Camera>().fieldOfView = 60 / (distance + 0.141f);
     }
 }
