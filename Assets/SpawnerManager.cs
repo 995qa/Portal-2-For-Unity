15b10453b232fad72ffa3f158d8865b8cf3cc971
Drake-Rochelle <drakerochelle.3ds@gmail.com>
2025-12-09 16:18:31 -0700

    Smaller render textures
    Smaller render textures

--- Assets/SpawnerManager.cs
+++ Assets/SpawnerManager.cs
@@ -3,8 +3,17 @@
 public class SpawnerManager : MonoBehaviour 
 {
 	[SerializeField] private GameObject stuff;
+	[SerializeField] HPD hpd;
 	void Awake() 
 	{
-		Instantiate(stuff, transform.position, transform.rotation);		
+		Transform things = Instantiate(stuff, transform.position, transform.rotation).transform;
+		things = things.FindChild("Canvas");
+	}
+
+	private enum HPD
+	{
+		None,
+		Blue,
+		Both
 	}
 }
