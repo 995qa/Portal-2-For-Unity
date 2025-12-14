5dbf8d7d60e3f14063551d28e797b083a7d17ea8
995qa <zackerycole28@gmail.com>
2025-12-14 15:35:26 -0500

    "IsraelGPT, ruin the movement script because I was too lazy to do it myself"

--- Assets/Scripts/PlayerController3D.cs
+++ Assets/Scripts/PlayerController3D.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class PlayerController3D : MonoBehaviour
+public class PlayerController3D : MonoBehaviour 
 {
     [SerializeField] private Transform cam;
     [SerializeField] private float accelSpeed;
@@ -15,104 +15,143 @@ public class PlayerController3D : MonoBehaviour
     [SerializeField] private AudioClip[] wAudioClips;
     [SerializeField] private AudioClip[] bAudioClips;
     [SerializeField] private float vol;
-    [SerializeField] private float dur;
-
     private Rigidbody rb;
     private Transform player;
     private float angle;
     private bool grounded;
-    private bool playing;
-    private bool move;
-
     private Material currOn;
+    private bool playing;
+    [SerializeField] private float dur;
+    private bool move;
     private List<AudioClip>[] audioClips;
-
-    public static PlayerController3D Instance { get; private set; }
-
+    public static PlayerController3D Instance
+    {
+        get; private set;
+    }
     private void Awake()
     {
         if (Instance != null)
         {
-            Debug.LogWarning("More than one " + name + ", ya chump");
+            Debug.LogWarning("More than one " + this.name + ", ya chump");
             Destroy(gameObject);
-            return;
         }
-
-        Instance = this;
+        else
+        {
+            Instance = this;
+        }
         player = transform;
         rb = GetComponent<Rigidbody>();
-
         audioClips = new List<AudioClip>[2];
         audioClips[0] = wAudioClips.ToList();
         audioClips[1] = bAudioClips.ToList();
     }
-
-    void Update()
-    {
-        HandleLook();
-        HandleMovement();
-        HandleGroundCheck();
-    }
-
-    private void HandleLook()
-    {
+    void Update () 
+	{
         angle = cam.rotation.eulerAngles.x;
-        if (angle > 180) angle -= 360;
-
- //       if (Input.GetMouseButton(0))
- //       {
-            float mouseX = Input.GetAxis("Mouse X") * lookSpeed * 2f;
-            float mouseY = Input.GetAxis("Mouse Y") * lookSpeed * 2f;
-
-            player.rotation = Quaternion.Euler(
-                0f,
-                player.rotation.eulerAngles.y + mouseX,
-                0f
-            );
-
-            float nextCamAngle = angle - mouseY;
-            nextCamAngle = Mathf.Clamp(nextCamAngle, -90f, 90f);
-
-            cam.rotation = Quaternion.Euler(
-                nextCamAngle,
-                cam.rotation.eulerAngles.y,
-                0f
-            );
-//        }
-
-#if UNITY_EDITOR
-        float camAngle = 0f;
+        if (angle > 180)
+        {
+            angle = angle - 360;
+        }
+        if (angle - (Input.GetAxis("Mouse Y") * lookSpeed * 2) < -90)
+        {
+            cam.rotation = Quaternion.Euler(-90f, cam.rotation.eulerAngles.y, cam.rotation.eulerAngles.z);
+        }
+        else if (angle - (Input.GetAxis("Mouse Y") * lookSpeed * 2) > 90)
+        {
+            cam.rotation = Quaternion.Euler(90f, cam.rotation.eulerAngles.y, cam.rotation.eulerAngles.z);
+        }
+        else
+        {
+ //           if (Input.GetMouseButton(0))
+ //           {
+                player.rotation = Quaternion.Euler(
+                    new Vector3(
+                        player.rotation.eulerAngles.x,
+                        player.rotation.eulerAngles.y + (Input.GetAxis("Mouse X") * lookSpeed * 2),
+                        player.rotation.eulerAngles.z
+                        )
+                    );
+                cam.rotation = Quaternion.Euler(
+                    new Vector3(
+                        cam.rotation.eulerAngles.x - (Input.GetAxis("Mouse Y") * lookSpeed * 2),
+                        cam.rotation.eulerAngles.y,
+                        cam.rotation.eulerAngles.z
+                        )
+                    );
+ //           }
+        }
+		
+		float camAngle = 0f;
         float playerAngle = 0f;
-
-        if (Input.GetKey(KeyCode.RightArrow)) playerAngle += lookSpeed / 5f;
-        if (Input.GetKey(KeyCode.LeftArrow)) playerAngle -= lookSpeed / 5f;
-        if (Input.GetKey(KeyCode.UpArrow)) camAngle += lookSpeed / 5f;
-        if (Input.GetKey(KeyCode.DownArrow)) camAngle -= lookSpeed / 5f;
-
+		
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerAngle += lookSpeed / 5;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerAngle -= lookSpeed / 5;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            camAngle += lookSpeed / 5;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            camAngle -= lookSpeed / 5;
+        }
         rb.freezeRotation = false;
-        player.rotation = Quaternion.Euler(0f, player.rotation.eulerAngles.y + playerAngle, 0f);
+        player.rotation = Quaternion.Euler(player.rotation.eulerAngles.x, player.rotation.eulerAngles.y + playerAngle, player.rotation.eulerAngles.z);
         rb.freezeRotation = true;
-
-        float nextAngle = Mathf.Clamp(angle - camAngle, -90f, 90f);
-        cam.rotation = Quaternion.Euler(nextAngle, cam.rotation.eulerAngles.y, 0f);
-#endif
-    }
-
-    private void HandleMovement()
-    {
+        angle = cam.rotation.eulerAngles.x;
+        if (angle > 180)
+        {
+            angle = angle - 360;
+        }
+        if (angle - camAngle < -90)
+        {
+            cam.rotation = Quaternion.Euler(-90f, cam.rotation.eulerAngles.y, cam.rotation.eulerAngles.z);
+        }
+        else if (angle - camAngle > 90)
+        {
+            cam.rotation = Quaternion.Euler(90f, cam.rotation.eulerAngles.y, cam.rotation.eulerAngles.z);
+        }
+        else
+        {
+            cam.rotation = Quaternion.Euler(cam.rotation.eulerAngles.x - camAngle, cam.rotation.eulerAngles.y, cam.rotation.eulerAngles.z);
+        }
         Vector2 horVel = new Vector2(rb.velocity.x, rb.velocity.z);
-
-        float forwardForce = 0f;
-        float sidewaysForce = 0f;
+        float  forwardForce  = 0f;
+        float  sidewaysForce = 0f;
         move = false;
-
-        if (Input.GetKey(KeyCode.W)) { forwardForce += Time.deltaTime; move = true; }
-        if (Input.GetKey(KeyCode.S)) { forwardForce -= Time.deltaTime; move = true; }
-        if (Input.GetKey(KeyCode.D)) { sidewaysForce += Time.deltaTime; move = true; }
-        if (Input.GetKey(KeyCode.A)) { sidewaysForce -= Time.deltaTime; move = true; }
-
-        PortalGunAnimator.Instance.animate = move;
-
+        if (Input.GetKey(KeyCode.W))
+        {
+            forwardForce += 1 * Time.deltaTime;
+            move = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            forwardForce -= 1 * Time.deltaTime;
+            move = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            sidewaysForce += 1 * Time.deltaTime;
+            move = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            sidewaysForce -= 1 * Time.deltaTime;
+            move = true;
+        }
+        if (forwardForce!=0 || sidewaysForce != 0)
+        {
+            PortalGunAnimator.Instance.animate = true;
+        }
+        else
+        {
+            PortalGunAnimator.Instance.animate = false;
+        }
         if (horVel.magnitude < maxSpeed)
         {
             rb.velocity += transform.forward * forwardForce * accelSpeed;
@@ -123,69 +162,47 @@ public class PlayerController3D : MonoBehaviour
             horVel = horVel.normalized * maxSpeed;
             rb.velocity = new Vector3(horVel.x, rb.velocity.y, horVel.y);
         }
-
-        if (!move && grounded)
-        {
-            rb.velocity = new Vector3(
-                rb.velocity.x / (friction * Time.deltaTime),
-                rb.velocity.y,
-                rb.velocity.z / (friction * Time.deltaTime)
-            );
-        }
-    }
-
-    private void HandleGroundCheck()
-    {
-        grounded = false;
         RaycastHit hit;
-
-        float radius = GetComponentInChildren<CapsuleCollider>().radius - 0.05f;
-
-        if (Physics.SphereCast(transform.position, radius, Vector3.down, out hit, raycastDistance))
+        if ((sidewaysForce == 0 && forwardForce == 0) && grounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x / (friction * Time.deltaTime), rb.velocity.y, rb.velocity.z / (friction * Time.deltaTime));
+        }
+        grounded = false;
+        if (Physics.SphereCast(transform.position, gameObject.GetComponentInChildren<CapsuleCollider>().radius-0.05f, new Vector3(0,-1,0), out hit, raycastDistance))
         {
             if (hit.transform.parent != null)
             {
-                if (hit.transform.parent.name.Contains("lack"))
+                if (hit.transform.parent.gameObject.name.Contains("lack"))
+                {
                     currOn = Material.Black;
-                else if (hit.transform.parent.name.Contains("hite"))
-                    currOn = Material.White;
+                }
+                else if (hit.transform.parent.gameObject.name.Contains("hite"))
+                {
+                    currOn = Material.Black;
+                }
                 else
+                {
                     currOn = Material.None;
+                }
             }
-
             if (hit.collider.gameObject.layer != LayerMask.NameToLayer("Portal Walls"))
+            {
                 grounded = true;
-
+            }
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                AudioManager.Instance.Play(
-                    transform,
-                    audioClips[(int)currOn][audioClips[(int)currOn].Count - 1],
-                    AudioManager.Mixer.SFX,
-                    vol * 2,
-                    true,
-                    transform.position,
-                    false
-                );
-
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                AudioManager.Instance.Play(transform, audioClips[(int)currOn][audioClips[(int)currOn].Count - 1], AudioManager.Mixer.SFX, vol * 2, true, transform.position, false);
+                rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
             }
         }
-
-        if (grounded && move && !playing && currOn != Material.None)
+        if (grounded && move && !playing)
         {
-            playing = true;
-            AudioManager.Instance.Play(
-                transform,
-                audioClips[(int)currOn][Random.Range(0, audioClips[(int)currOn].Count - 1)],
-                AudioManager.Mixer.SFX,
-                vol,
-                true,
-                transform.position,
-                false
-            );
-
-            StartCoroutine(Wait());
+            if (currOn != Material.None)
+            {
+                playing = true;
+                AudioManager.Instance.Play(transform, audioClips[(int)currOn][Random.Range(0, audioClips[(int)currOn].Count - 1)], AudioManager.Mixer.SFX, vol, true, transform.position, false);
+                StartCoroutine(Wait());
+            }
         }
     }
 
@@ -194,11 +211,10 @@ public class PlayerController3D : MonoBehaviour
         yield return new WaitForSeconds(dur);
         playing = false;
     }
-
     public enum Material
     {
         None,
         Black,
-        White
+        White,
     }
 }
